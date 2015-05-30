b0a1276daddc62c9074dd48d9292f476a40b47d0
Jana Hehr <janahehr@yahoo.de>
2015-05-30 19:04:09 +0200

    restructure public/server structure-->prepare for gulp build

--- Source/server/server.js
+++ Source/server/server.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var http = require("http").Server(app);
 
-app.use("/", express.static(__dirname + "/public"));
+app.use("/", express.static(__dirname + "/../public"));
 app.use("/", bodyParser.urlencoded(
 {
     extended: false
@@ -15,7 +15,7 @@ require("./routes")(app);
 
 app.get("/*", function(request, response)
 {
-    response.sendFile(__dirname + "/public/index.html");
+    response.sendFile(__dirname + "/../public/index.html");
 });
 
 http.listen(port, function()
