ae165e37564a23b2d2c8ecaba8e34405eab8b23a
Jana Hehr <janahehr@yahoo.de>
2015-05-31 13:49:57 +0200

    merge path changes

--- Source/server/server.js
+++ Source/server/server.js
@@ -4,8 +4,11 @@ var app = express();
 var bodyParser = require("body-parser");
 var http = require("http").Server(app);
 var path = require("path");
+var config = require("./config.json")[process.env.NODE_ENV || 'development'];
 
-app.use("/", express.static(path.resolve(__dirname + "/../public")));
+console.log('using ' + config.publicFilePath + ' to serve public files');
+
+app.use("/", express.static(path.resolve(__dirname + config.publicFilePath)))
 app.use("/", bodyParser.urlencoded(
 {
     extended: false
@@ -16,7 +19,7 @@ require("./routes")(app);
 
 app.get("/*", function(request, response)
 {
-    response.sendFile(path.resolve(__dirname + "/../public/index.html"));
+    response.sendFile(path.resolve(__dirname + config.publicFilePath + "/index.html"));
 });
 
 
