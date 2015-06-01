599076ba0f11d7d909f3bb6d824f6f33d7b4f2f1
Jana Hehr <janahehr@yahoo.de>
2015-06-01 21:30:28 +0200

    Testdata for mock.Service. ng-bind on id field for better list performance

--- Source/server/services/knowledgeBlogService.mock.js
+++ Source/server/services/knowledgeBlogService.mock.js
@@ -1,34 +1,43 @@
 "use strict";
 
-exports.createBlogEntry = function(title, author, content) {
+exports.createBlogEntry = function (title, author, content) {
 
 
 };
 
-
-exports.getAllBlogEntries = function()
+exports.getAllBlogEntries = function(callback)
 {
     var mock = [];
 
-    mock.push(
-    {
-        title: "Ein super langer Titel, der nichts sagend daher kommt und  blubb blubb möglicherweise alle Grenzen sprengt oder auch einfach abgeschnitten oder 'ellipsiert' wird (dann fehlt hat was vom langen Text)",
-        author: "Thomas Klepzig",
-        content: "# test"
-    });
-    mock.push(
-    {
-        title: "Hier haben wir einen etwas längeren Titel, der immer noch hinpasst",
-        author: "Thomas Klepzig",
-        content: "Blubberbrause2"
-    });
-    mock.push(
-    {
-        title: "Was kurzes",
-        author: "Thomas Klepzig",
-        content: "Blubberbrause3"
-    });
+    for (var i = 0; i < 1000; i++) {
+          mock.push(
+            {
+                _id: i,
+                title: "Ein super langer Titel, " + i,
+                author: "Thomas Klepzig",
+                content: "# test"
+            });
+    }
 
-
-    return mock;
+    // mock.push(
+    // {
+    //     title: "Ein super langer Titel, der nichts sagend daher kommt und  blubb blubb möglicherweise alle Grenzen sprengt oder auch einfach abgeschnitten oder 'ellipsiert' wird (dann fehlt hat was vom langen Text)",
+    //     author: "Thomas Klepzig",
+    //     content: "# test"
+    // });
+    // mock.push(
+    // {
+    //     title: "Hier haben wir einen etwas längeren Titel, der immer noch hinpasst",
+    //     author: "Thomas Klepzig",
+    //     content: "Blubberbrause2"
+    // });
+    // mock.push(
+    // {
+    //     title: "Was kurzes",
+    //     author: "Thomas Klepzig",
+    //     content: "Blubberbrause3"
+    // });
+    //
+    callback(mock);
+    //return mock;
 };
