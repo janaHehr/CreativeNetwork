5175b659b0b233790e81aabd0ac7b63d62c84311
Jana Hehr <janahehr@yahoo.de>
2015-06-01 21:33:59 +0200

    refactor with JsLinter tipps

--- Source/public/app/components/syncScroll/syncScroll.js
+++ Source/public/app/components/syncScroll/syncScroll.js
@@ -1,34 +1,33 @@
+'use strict';
 angular.module('comparex.syncScroll', [])
-.directive('syncScroll', function() {
-    var containerId;
-    var markdownId;
+    .directive('syncScroll', function () {
+        var containerId, markdownId;
 
-    var syncScroll = function(){
-      var codeViewport = document.querySelector('.CodeMirror-scroll'),
-      previewViewport = document.getElementById(containerId),
-      codeContent = document.querySelector('.CodeMirror-sizer'),
-      previewContent = document.getElementById(markdownId),
+        var syncScroll = function () {
+            var codeViewport = document.querySelector('.CodeMirror-scroll'),
+                previewViewport = document.getElementById(containerId),
+                codeContent = document.querySelector('.CodeMirror-sizer'),
+                previewContent = document.getElementById(markdownId),
 
-      codeHeight = codeContent.clientHeight - window.getComputedStyle(codeViewport, null).height.split("px")[0],
-      previewHeight = previewContent.clientHeight - window.getComputedStyle(previewViewport, null).height.split("px")[0],
-      ratio = previewHeight / codeHeight,
-      previewPostition = codeViewport.scrollTop * ratio;
+                codeHeight = codeContent.clientHeight - window.getComputedStyle(codeViewport, null).height.split('px')[0],
+                previewHeight = previewContent.clientHeight - window.getComputedStyle(previewViewport, null).height.split('px')[0],
+                ratio = previewHeight / codeHeight,
+                previewPostition = codeViewport.scrollTop * ratio;
+            previewViewport.scrollTop = previewPostition;
+        };
 
-      previewViewport.scrollTop = previewPostition;
-    };
+        return {
+            restrict: 'A',
+            scope: {
+                previewContainer: '=',
+                markdownContainer: '='
+            },
+            link: function (scope) {
+                containerId = scope.previewContainer;
+                markdownId = scope.markdownContainer;
 
-    return {
-      restrict: 'A',
-      scope: {
-        previewContainer: '=',
-        markdownContainer: '='
-      },
-      link: function(scope, element) {
-        containerId = scope.previewContainer;
-        markdownId = scope.markdownContainer;
-
-        var codemirrorScroll = document.getElementsByClassName('CodeMirror-scroll')[0];
-       codemirrorScroll.onscroll = syncScroll;
-      }
-    };
-  });
+                var codemirrorScroll = document.getElementsByClassName('CodeMirror-scroll')[0];
+                codemirrorScroll.onscroll = syncScroll;
+            }
+        };
+    });
