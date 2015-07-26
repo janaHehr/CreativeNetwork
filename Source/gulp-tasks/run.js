0902310496d6f62b7154c93b4e286665a72338c5
unknown <HehrJ@DEECB1D7979A82.pcw.local>
2015-07-26 16:44:06 +0200

    add gulp webserver for serving static files

--- Source/gulp-tasks/run.js
+++ Source/gulp-tasks/run.js
@@ -2,6 +2,7 @@
 
 module.exports = function(gulp, plugins, config) {
 
+  //express node-sserver for serving api
   gulp.task('server', function() {
     plugins.nodemon({
       script: 'server/server.js',
@@ -12,4 +13,16 @@ module.exports = function(gulp, plugins, config) {
     });
   });
 
+  //webserver for serving static content
+  gulp.task('webserver', function() {
+    gulp.src('dist/public')
+      .pipe(plugins.webserver({
+        livereload: true,
+        open: true,
+        proxies: [
+          { source: '/api', target: 'http://localhost:51104/api' }
+        ],
+        fallback:'index.html'
+      }));
+  });
 };
--- Source/server/server.js
+++ Source/server/server.js
@@ -6,11 +6,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 var path = require('path');
-var config = require('./config.json')[process.env.NODE_ENV || 'development'];
 
-console.log('using ' + config.publicFilePath + ' to serve public files');
-
-app.use('/', express.static(path.resolve(__dirname + config.publicFilePath)));
 app.use('/', bodyParser.urlencoded(
 {
     extended: false
@@ -18,11 +14,6 @@ app.use('/', bodyParser.urlencoded(
 app.use(bodyParser.json());
 
 require('./routes')(app);
-
-app.get('/*', function(request, response)
-{
-    response.sendFile(path.resolve(__dirname + config.publicFilePath + '/index.html'));
-});
 
 http.listen(process.env.PORT || port, function()
 {
